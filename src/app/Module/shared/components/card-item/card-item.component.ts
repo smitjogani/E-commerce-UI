fe822ba0fdad489741e96edd3814d85f15be6eaf
smit <smitjogani00@gmail.com>
2024-02-20 23:50:18 +0530

    UI

---
+++ src/app/Module/shared/components/card-item/card-item.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './card-item.component.html',
+  styleUrl: './card-item.component.css'
+})
+export class CardItemComponent {
+  updateCartItem(num:Number){
+    console.log(num);
+  }
+}

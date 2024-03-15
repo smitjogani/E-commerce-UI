0acc55c78dbbb11331ea49b663fa562d2ca66e02
smit <smitjogani00@gmail.com>
2024-03-15 19:00:27 +0530

    __

--- Frontend/src/app/State/Cart/cart.action.ts
+++ Frontend/src/app/State/Cart/cart.action.ts
@@ -26,3 +26,31 @@ export const getCartFailure = createAction(
   '[Cart] get cart failure',
   props<{ error: any }>()
 );
+
+export const removeCartItemRequest = createAction(
+  '[Cart] get remove cart item request'
+);
+
+export const removeCartItemSuccess = createAction(
+  '[Cart] cart item removed',
+  props<{ cartItemId: any }>()
+);
+
+export const removeCartItemFailure = createAction(
+  '[Cart] get cart removing fail',
+  props<{ error: any }>()
+);
+
+export const updateCartItemRequest = createAction(
+  '[Cart] get update cart item request'
+);
+
+export const updateCartItemSuccess = createAction(
+  '[Cart] update cart item success',
+  props<{ payload: any }>()
+);
+
+export const updateCartItemFailure = createAction(
+  '[Cart] update cart item failure',
+  props<{ error: any }>()
+);
--- Frontend/src/app/State/Cart/cart.service.ts
+++ Frontend/src/app/State/Cart/cart.service.ts
@@ -8,6 +8,10 @@ import {
   addItemToCartSuccess,
   getCartFailure,
   getCartSuccess,
+  removeCartItemFailure,
+  removeCartItemSuccess,
+  updateCartItemFailure,
+  updateCartItemSuccess,
 } from './cart.action';
 import { catchError, map, of } from 'rxjs';
 
@@ -93,12 +97,41 @@ export class cartService {
       .delete(url, { headers })
       .pipe(
         map((data: any) => {
-          console.log('added item : ', data);
-          return addItemToCartSuccess({ payload: data });
+          console.log('Remove Cart Item : ', data);
+          return removeCartItemSuccess({ cartItemId });
+        }),
+        catchError((error: any) => {
+          return of(
+            removeCartItemFailure(
+              error.response && error.response.data.message
+                ? error.response.data.message
+                : error.message
+            )
+          );
+        })
+      )
+      .subscribe((action) => {
+        this.store.dispatch(action);
+      });
+  }
+
+  updateCartItem(reqData: any) {
+    const url = `${this.API_BASE_URL}/api/cart_items/${reqData.cartItemId}`;
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${localStorage.getItem('jwt')}`,
+      'Content-Type': 'application/json',
+    });
+
+    return this.http
+      .put(url, reqData, { headers })
+      .pipe(
+        map((data: any) => {
+          console.log('updated data : ', data);
+          return updateCartItemSuccess({ payload: data });
         }),
         catchError((error) => {
           return of(
-            addItemToCartFailure(
+            updateCartItemFailure(
               error.response && error.response.data.message
                 ? error.response.data.message
                 : error.message
